dea9a921daeebd9198584c87798f446ccb3ff5ba
Dylan Halperin <dylan@dylemma.io>
2015-05-02 16:31:48 -0400

    Move player's center to the tip, tweak the following angle

--- newclient/src/js/lib/Player.js
+++ newclient/src/js/lib/Player.js
@@ -1,8 +1,8 @@
 var Vec = require('victor')
 
 var playerScale = 1.4
-var playerRx = playerScale * 1
-var playerRy = playerScale * 5 / 3
+var playerHalfWidth = playerScale * 1
+var playerHeight = playerScale * 10 / 3
 
 /*
 @param target {Vec}
@@ -14,8 +14,8 @@ function Player(target, bounds){
 	this.position = target.clone()
 	this.rotation = 0
 
-	// circular buffer containing the last 5 'dx' values
-	this._recentDx = [0, 0, 0, 0, 0]
+	// circular buffer containing the last 3 'dx' values
+	this._recentDx = [0, 0, 0]
 	this._recentDxIndex = 0
 }
 
@@ -31,13 +31,13 @@ Player.prototype.followTarget = function(){
 
 	// insert the movement value into the _recentDx array
 	this._recentDx[this._recentDxIndex++] = movedX
-	this._recentDxIndex = this._recentDxIndex % 5
+	this._recentDxIndex = this._recentDxIndex % 3
 
 	// sum the _recentDx values
 	var recentMovement = this._recentDx.reduce(function(a,b){ return a + b })
 
 	// pick an angle based on the recent movement
-	var angle = (Math.abs(recentMovement) > 0.1) ? recentMovement : 0
+	var angle = (Math.abs(recentMovement) > 0.1) ? recentMovement * 2 : 0
 	angle = Math.max(-90, Math.min(90, angle))
 	this.rotation = angle * Math.PI / 180
 }
@@ -48,14 +48,14 @@ Player.prototype.draw = function(context, invScale){
 	context.rotate(this.rotation)
 
 	context.beginPath()
-	context.moveTo(0, -playerRy)
-	context.lineTo(playerRx, playerRy)
-	context.quadraticCurveTo(0, playerRy * 0.4, -playerRx, playerRy)
+	context.moveTo(0, 0)
+	context.lineTo(playerHalfWidth, playerHeight)
+	context.quadraticCurveTo(0, playerHeight * 0.8, -playerHalfWidth, playerHeight)
 	context.closePath()
 
 	context.fillStyle = '#fff'
 	context.strokeStyle = '#000'
-	context.lineWidth = invScale // playerScale * 0.2
+	context.lineWidth = invScale
 	context.fill()
 	context.stroke()
 
